b2b78632b9902a858a2a17f2684228569ce742f6
acomanda <acomanda@students.uni-mainz.de>
2019-09-18 12:33:21 +0200

    Written function for creating the database in the DatabaseHandler class. Deleted the old database

--- app/src/main/java/com/example/geogeo/DatabaseHandler.java
+++ app/src/main/java/com/example/geogeo/DatabaseHandler.java
@@ -1,9 +1,23 @@
 package com.example.geogeo;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 
 import java.util.ArrayList;
 
 public class DatabaseHandler {
+
+    SQLiteDatabase db;
+
+    DatabaseHandler() {
+        db = SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.geogeo/databases/db.db", null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS Answer(Id INTEGER, Answer TEXT, X Numeric, Y Numeric)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS Game(Id INTEGER, Amount INTEGER, Points INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS GameQuestions(GameId INTEGER, QuestionId INTEGER, Points INTEGER, " +
+                "AnswerX NUMERIC, AnswerY NUMERIC)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS PicQuestions(Id INTEGER, Pic BLOB, AnswerId INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS Statistics(Id INTEGER, Games INTEGER, AverageScore INTEGER, TotalPoints INTEGER)");
+    }
+
 
     public int getRandomPicQuestion(ArrayList<Integer> blacklist){
         return 1;
