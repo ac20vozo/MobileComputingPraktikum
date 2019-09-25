06938c12ce8bb7121f23b983cf88c01c710dd8d5
Mo <m.alkrad@hotmail.com>
2019-09-26 01:14:39 +0200

    Code fixes.

--- app/src/main/java/com/example/geogeo/LoginActivity.java
+++ app/src/main/java/com/example/geogeo/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +16,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.j256.ormlite.stmt.query.In;
 
 public class LoginActivity extends AppCompatActivity {
     public EditText emailId, password;
